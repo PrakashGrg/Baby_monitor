3944c38be11b73b937476c0fa1af320a5808c684
PrakashGrg <rabig6885@gmail.com>
2026-08-14 12:29:05 +0545

    Switch database from SQLite to PostgreSQL

--- babycare_backend/babycare_backend/settings.py
+++ babycare_backend/babycare_backend/settings.py
@@ -5,6 +5,7 @@ Django settings for babycare_backend project.
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,12 +73,19 @@ CHANNEL_LAYERS = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = config('DATABASE_URL', default='')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_USER_MODEL = 'accounts.User'
 
